8b6f3c878ead4bf0ffd1f36aeb87a800d1280425
Roman Sysoev <romansysoev492@gmail.com>
2026-03-05 12:00:52 +0300

    correction

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from application.salary import calculate_salary
-from application.db.peaple import get_employees
+from application.db.people import get_employees
 from datetime import date
 from rich import print
 
-today = date.today()
+
 
 if __name__ == '__main__':
+    
+    today = date.today()
     print(f'Текущая дата: {today}')
     calculate_salary()
     get_employees()
